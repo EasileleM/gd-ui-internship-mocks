b2f908cd49493bc678c652a952428db649a55055
EasileleM <pro100prosavva@gmail.com>
2019-10-15 16:17:01 +0400

    *fix: fixed disable button logic

--- doc3/public/scripts/registation-form-button-disable.js
+++ doc3/public/scripts/registation-form-button-disable.js
@@ -9,8 +9,8 @@ function formIsValid(input) {
   const currentForm = documentForms[input.getAttribute("form")];
   const currentSumbitButton = currentForm.getElementsByTagName("button")[0];
   if (currentForm.checkValidity()) {
-    currentSumbitButton.setAttribute("disabled", "");
-  } else if (currentSumbitButton.hasAttribute("disabled")) {
     currentSumbitButton.removeAttribute("disabled");
+  } else if (!currentSumbitButton.hasAttribute("disabled")) {
+    currentSumbitButton.setAttribute("disabled", "");
   }
 }
