81be0a2efeecd1f2e745cd5ad7631e761b4573b9
EasileleM <pro100prosavva@gmail.com>
2019-10-15 16:13:01 +0400

    *refactor: changed button disabled logic

--- doc3/public/scripts/registation-form-button-disable.js
+++ doc3/public/scripts/registation-form-button-disable.js
@@ -1,63 +1,16 @@
-const formButtonsContainers = Array
-  .prototype
-  .map
-  .call(document.getElementsByClassName('registration-button'),
-    (button) => {
-      const buttonContainer = { button };
-
-      let parentElement = button.parentElement;
-      while (parentElement && parentElement.tagName.toLowerCase() != 'form') {
-        parentElement = parentElement.parentElement;
-      }
-      if (!parentElement) {
-        return buttonContainer;
-      }
-
-      buttonContainer.inputs = parentElement.getElementsByTagName('input');
-      buttonContainer.validInputCounter = 0;
-      buttonContainer.inputValidity = Array
-        .prototype
-        .map
-        .call(buttonContainer.inputs, ((input) => {
-          let currentInputValidity = input.checkValidity();
-          currentInputValidity && buttonContainer.validInputCounter++;
-          return currentInputValidity;
-        }));
-      
-      return buttonContainer;
-    });
-formButtonsContainers
-  .forEach((buttonContainer) => changeButtonState(buttonContainer.button));
-
-function inputIsValid(input) {
-  let parentElement = input.parentElement;
-  while (parentElement.tagName.toLowerCase() != 'form') {
-    parentElement = input.parentElement;
+const documentForms = document.forms;
+for (const form of documentForms) {
+  if (form.checkValidity()) {
+    currButtonContainer.button.removeAttribute("disabled");
   }
-  const currentButton = parentElement.getElementsByClassName('registration-button')[0];
-  const currButtonContainer = formButtonsContainers
-    .find((buttonContainer) => buttonContainer.button === currentButton);
-  const currInputValidityIndex = Array.prototype.indexOf
-    .call(currButtonContainer.inputs, input);
-  const currInputValidity = input.checkValidity();
-  if (!currButtonContainer.inputValidity[currInputValidityIndex] && currInputValidity) {
-    currButtonContainer.validInputCounter++;
-  } else if (currButtonContainer.inputValidity[currInputValidityIndex] && !currInputValidity) {
-    currButtonContainer.validInputCounter--;
-  }
-  currButtonContainer.inputValidity[currInputValidityIndex] = currInputValidity;
-  changeButtonState(currButtonContainer.button);
 }
 
-function changeButtonState(button) {
-  const currButtonContainer = formButtonsContainers
-    .find((buttonContainer) => buttonContainer.button === button);
-  if (!currButtonContainer.inputs) {
-    return;
-  }
-  if (currButtonContainer.validInputCounter == currButtonContainer.inputs.length) {
-    currButtonContainer.button.removeAttribute("disabled");
-  } else {
-    currButtonContainer.button.setAttribute("disabled", "");
+function formIsValid(input) {
+  const currentForm = documentForms[input.getAttribute("form")];
+  const currentSumbitButton = currentForm.getElementsByTagName("button")[0];
+  if (currentForm.checkValidity()) {
+    currentSumbitButton.setAttribute("disabled", "");
+  } else if (currentSumbitButton.hasAttribute("disabled")) {
+    currentSumbitButton.removeAttribute("disabled");
   }
 }
